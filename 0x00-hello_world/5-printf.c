3e1bd447b2681ad965f69c9ce08c74ca3f72e646
ondi20 <andyondigo@gmail.com>
2023-07-14 13:03:12 -0700

    Solution for the 0x00-hello_world c program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -A program that prints a line usinf printf funtion
+ * * Return: 0(success)
+ */
+
+int main(void)
+{
+prinf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+This is part one project 0x00-hello_world c programming
